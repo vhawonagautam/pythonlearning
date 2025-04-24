2cb9fb8b0c9b782e083711820f59dc640ff98a4d
Vhawona Gautam <vhawonagooutam@gmail.com>
2025-04-24 11:52:35 +0545

    solved problem5

--- problem5.py
+++ problem5.py
@@ -1,7 +1,14 @@
 def main():
     number = input("Enter any number:")
-    for i in range(5):
-        print(number[(4-i):(5+i)])
+    length = len(number)
+    mid = length // 2
+    
+
+    for i in range(mid+1):
+        line = number[(mid-i):(mid + i +1)]
         
+        spaces = ' ' * (mid - i) 
         
+        print(spaces + line)   
+         
 main()
